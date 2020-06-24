e9db71c3fdb2e7ded7a99accc83256e72b2b1b3f
Thiago Silva <thiagodd.silva@gmail.com>
2020-06-23 21:02:25 -0300

    split home page in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './pages/_layout/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './pages/_layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
